899d47a5e0ff5a64402280917f3533ed2bb7c01a
arrazola2099 <arrazola2099@gmail.com>
2015-12-11 18:39:39 -0200

    Fixing rspec tests

--- spec/tabla_spec.rb
+++ spec/tabla_spec.rb
@@ -1,10 +1,17 @@
 require "./lib/Tablero"
 
 describe "Tablero" do
+    before(:all) do
+        @tb = Tablero.new
+    end
 	it "should generate an empty table" do
-	tb = Tablero.new
-	str = tb.display()
-	expect(str).to eq("<table border=\"solid black\"><tr><td ></td><td ></td><td ></td></tr><tr><td ></td><td ></td><td ></td></tr><tr><td ></td><td ></td><td ></td></tr></table>")
+	    str = @tb.display()
+    	expect(str).to include("<table border=\"solid black\">")
+	end
+	
+	it "should have a form for cell 7" do
+	    str = @tb.display()
+	    expect(str).to include("id=\"cell7\"")
 	end
 
 end
